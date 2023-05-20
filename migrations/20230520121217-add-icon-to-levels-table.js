638124d5391e68fe59fb6f11807e9fde38789ed9
kanhai <kanhailal2010@gmail.com>
2023-05-20 17:56:39 +0530

    add icon column to levels

---
+++ migrations/20230520121217-add-icon-to-levels-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('levels', 'icon', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        after: 'name',
+        defaultValue: null
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('levels', 'icon');
+  }
+};
