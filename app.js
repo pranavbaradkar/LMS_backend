b8a8a829633ccaf8c3a632af290f8f179b368664
rajesh <rajesh@octillion.tv>
2023-06-03 15:34:03 +0530

    fix large size

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const fs = require('fs');
 const path = require('path')
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit:'50mb'}));
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
