6f79cfa320a5b5e2f674d2ac0a9c66a2717d62ab
Rajesh <rajesh@octillion.tv>
2023-06-10 14:08:40 +0530

    Fix data

--- controllers/user/assessment.controller.js
+++ controllers/user/assessment.controller.js
@@ -114,7 +114,7 @@ const getScreeningTestDetails = async function (req, res) {
           [
             {
               model: questions,
-              attributes:['id','question_type', 'statement', 'mime_type','hint','difficulty_level','complexity_level','knowledge_level','proficiency_level','blooms_taxonomy','skill_id','estimated_time','correct_answer_score','level_id','tags','subject_id', 's3_asset_urls'],
+              attributes:['id','question_type', 'statement', 'mime_type','hint','difficulty_level','complexity_level','knowledge_level','proficiency_level','blooms_taxonomy','skill_id','estimated_time','correct_answer_score','level_id','tags','subject_id', 's3_asset_urls', 'grade_id',  'topic_id', 'sub_strand_id', 'strand_id', 'lo_ids'],
               include: [
                 { model: question_options },
                 { model: question_mtf_answers },
