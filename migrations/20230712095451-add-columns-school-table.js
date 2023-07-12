9f02f61a3e34725cc138de826111b735f8e08b18
kanhai <kanhailal2010@gmail.com>
2023-07-12 18:25:05 +0530

    hr_email in schools table migration

---
+++ migrations/20230712095451-add-columns-school-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return [
+      queryInterface.addColumn('schools', 'hr_email_1', { type: Sequelize.STRING, allowNull: true })
+    ]
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    
+    return [
+      queryInterface.removeColumn('schools', 'hr_email_1')
+    ];
+    
+  }
+};
